1e91a7fdc4e95c1613a2550212a05e0a2c2a9881
Adriana Garcia <adrianagarciarodriguezzz@gmail.com>
2024-03-16 13:39:46 +0100

    Refactor form styling and add new product form fields

--- laravel-M07UF1-project/resources/views/form.blade.php
+++ laravel-M07UF1-project/resources/views/form.blade.php
@@ -14,37 +14,40 @@
 <body>
     <header>
         <br/>
-        <div class="title-cataleg-productes">AÑADIR PRODUCTO</div>
+        <div class="title-cataleg-productes">AFEGIR PRODUCTE</div>
     </header>
     <div class="separacion"></div>
 
     <form method="POST" action="{{ route('afegirForm') }}" enctype='multipart/form-data' class="container">
         @csrf      
-        <div class="form-group">
-            <label for="nom">NOM DEL PRODUCTE</label>
-            <input type="text" name="nom" placeholder="Escriu el nom del producte que afegiràs" class="form-control mb-2" autofocus>
-        </div>
-        <div class="form-group">
-            <label for="descripcio">DESCRIPCIÓ DEL PRODUCTE</label>
-            <input type="text" name="descripcio" placeholder="Descripción del producto" class="form-control mb-2">
-        </div>
-        <div class="row">
-            <div class="form-group col-3">
-                <label for="preu">PREU DEL PRODUCTE</label>
-                <input type="number" name="preu" placeholder="Precio del producto" class="form-control mb-2">
+        <div class="form">
+            <div class="form-group">
+                <label for="nom">NOM DEL PRODUCTE</label>
+                <input type="text" name="nom" placeholder="Escriu el nom del producte que afegiràs" class="form-control mb-2" autofocus>
             </div>
-            <div class="form-group col-3">
-                <label for="stock">STOCK DEL PRODUCTE</label>
-                <input type="number" name="stock" placeholder="Stock del producto" class="form-control mb-2">
+            <div class="form-group">
+                <label for="descripcio">DESCRIPCIÓ DEL PRODUCTE</label>
+                <input type="text" name="descripcio" placeholder="Descripció del producte" class="form-control mb-2">
             </div>
-        </div>
-        <div class="form-group">
-            <label for="imatge">IMATGE DEL PRODUCTE</label>
-            <input type="file" name="imatge" placeholder="Imagen del producto" class="form-control mb-2">
-        </div>
-        <div>
-            <input type='submit' value='Crear nuevo producto' class="btn btn-primary btn-block mt-2">
-        </div>
+            <div class="row">
+                <div class="form-group form-group-small col-3">
+                    <label for="preu">PREU DEL PRODUCTE</label>
+                    <input type="number" name="preu" placeholder="Preu del producte" class="form-control form-control-small mb-2">
+                </div>
+                <div class="form-group form-group-small col-3">
+                    <label for="stock">STOCK DEL PRODUCTE</label>
+                    <input type="number" name="stock" placeholder="Stock del producte" class="form-control form-control-small mb-2">
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group form-group-image col-3">
+                    <label for="imatge">IMATGE DEL PRODUCTE</label>
+                    <input type="file" name="imatge" class="form-control form-control-small mb-2">
+                </div>
+                <div class="form-group form-group-small col-3">
+                    <input type='submit' value='AFEGIR' class="btn btn-primary btn-block btn-addProduct form-control-small">
+                </div>  
+            </div>
     </form>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
