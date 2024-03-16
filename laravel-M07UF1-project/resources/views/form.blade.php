c0cfd3f1273d6786a0386473951493e61bf83b77
Adriana Garcia <adrianagarciarodriguezzz@gmail.com>
2024-03-16 12:22:38 +0100

    Add new images, update environment variables, create and drop productes table, update form and styles CSS

--- laravel-M07UF1-project/resources/views/form.blade.php
+++ laravel-M07UF1-project/resources/views/form.blade.php
@@ -3,23 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de Producto</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Añadir producto</title>
+    <!-- Enlace a Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link  href="{{ asset('../resources/css/form.css') }}" rel="stylesheet">    
+    <link  href="{{ asset('../resources/css/styles.css') }}" rel="stylesheet">    
+
 </head>
 <body>
+    <header>
+        <br/>
+        <div class="title-cataleg-productes">AÑADIR PRODUCTO</div>
+    </header>
+    <div class="separacion"></div>
 
-<form method="POST" action="{{ route('afegirForm') }}" enctype='multipart/form-data' class="container">
-    @csrf        
-    <h1 class="text-center">Introduce la información del producto</h1>
-    <input type="text" name="nom" placeholder="Nombre del producto" class="form-control mb-2" autofocus>
-    <input type="text" name="descripcio" placeholder="Descripción del producto" class="form-control mb-2">
-    <input type="number" name="preu" placeholder="Precio del producto" class="form-control mb-2">
-    <input type="number" name="stock" placeholder="Stock del producto" class="form-control mb-2">
-    <input type="file" name="imatge" placeholder="Imagen del producto" class="form-control mb-2">
-
-    <input type='submit' value='Crear nuevo producto' class="btn btn-primary btn-block mt-2">
-</form>
+    <form method="POST" action="{{ route('afegirForm') }}" enctype='multipart/form-data' class="container">
+        @csrf      
+        <div class="form-group">
+            <label for="nom">NOM DEL PRODUCTE</label>
+            <input type="text" name="nom" placeholder="Escriu el nom del producte que afegiràs" class="form-control mb-2" autofocus>
+        </div>
+        <div class="form-group">
+            <label for="descripcio">DESCRIPCIÓ DEL PRODUCTE</label>
+            <input type="text" name="descripcio" placeholder="Descripción del producto" class="form-control mb-2">
+        </div>
+        <div class="row">
+            <div class="form-group col-3">
+                <label for="preu">PREU DEL PRODUCTE</label>
+                <input type="number" name="preu" placeholder="Precio del producto" class="form-control mb-2">
+            </div>
+            <div class="form-group col-3">
+                <label for="stock">STOCK DEL PRODUCTE</label>
+                <input type="number" name="stock" placeholder="Stock del producto" class="form-control mb-2">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="imatge">IMATGE DEL PRODUCTE</label>
+            <input type="file" name="imatge" placeholder="Imagen del producto" class="form-control mb-2">
+        </div>
+        <div>
+            <input type='submit' value='Crear nuevo producto' class="btn btn-primary btn-block mt-2">
+        </div>
+    </form>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
--- laravel-M07UF1-project/resources/views/main.blade.php
+++ laravel-M07UF1-project/resources/views/main.blade.php
@@ -5,37 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mostrar Productes</title>
     <!-- Enlace a Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('../resources/css/styles.css') }}" rel="stylesheet">
 </head>
 
 <body>
     <header>
-        <h1 class="text-center" >Supermercat</h1>
+        <h1 class="title-super text-center">SUPERMERCAT</h1>
+        <div class="title-cataleg-productes">CATÀLEG DE PRODUCTES</div>
     </header>
-    <div class="title-cataleg-productes">CATÀLEG DE PRODUCTES</div>
+    
     <div class="separacion"></div>
     <div class="container">        
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg ">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <!-- <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="fas fa-arrow-left"></i> Atrás</a>
-                    </li> -->
+                     <li class="nav-item">
+                        <a class="btn a-custom nav-link" href="#"><i class="fas fa-arrow-circle-left"></i> Enrere </a>
+                    </li>
                 </ul>
                 <div class="mx-auto order-0">
-                    <!-- <form class="form-inline my-2 my-lg-0" action="/buscar" method="GET">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Buscar productos" aria-label="Search" name="q">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-                    </form> -->
+                    <form class="form-inline my-2 my-lg-0" action="/buscar" method="GET">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Busca un producte: (tovalloletes, xampú, etc)" aria-label="Search" name="q">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                    </form> 
                 </div>
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a  class="btn a-custom" href="{{ route('afegirProducteForm') }}" > afegir producte </a>
+                        <a class="btn a-custom" href="{{ route('afegirProducteForm') }}">
+                            Afegir producte <i class="fas fa-plus"></i> 
+                        </a>
                     </li>
                 </ul>
             </div>
@@ -45,24 +49,27 @@
             @foreach ($productes as $producte)
                 <div class="col-sm-4">
                     <div class="card">
-                        <img src="{{ $producte->imatge }}" class="card-img-top" alt="{{ $producte->nom }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $producte->nom }}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">{{ $producte->descripcio }}</h6>
-                            <p class="card-text">Precio: {{ $producte->preu }}</p>
-                            <!-- <a class="btn a-editar" href="{{ route('afegirProducteForm', ['id'=>$producte->id]) }}"> Editar </a> -->
-                            <a class="btn a-editar" href="{{route('eliminarProductes', ['id'=>$producte->id])}}" onclick="event.preventDefault(); document.getElementById('eliminar-producte-form-{{ $producte->id }}').submit();">
+                        <a class="btn a-eliminar" href="{{route('eliminarProductes', ['id'=>$producte->id])}}" onclick="event.preventDefault(); document.getElementById('eliminar-producte-form-{{ $producte->id }}').submit();">
                             <img src="{{ asset('imatges/trash-can.png') }}" alt="Eliminar" width="20" height="20">
                             </a>
                             <form id="eliminar-producte-form-{{ $producte->id }}" action="{{ route('eliminarProductes') }}" method="POST" style="display: none;">
                                 @csrf
                                 <input type="hidden" name="id" value="{{ $producte->id }}">
                             </form>
-
+                        <img src="{{ $producte->imatge }}" class="card-img-top" alt="{{ $producte->nom }}">
+                        
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between">
+                                <p class="card-text-price">{{ $producte->preu }} €</p> 
+                                <p class="card-text-stock">STOCK: {{ $producte->stock }}</p>
+                            </div>
+                            <h5 class="card-title">{{ $producte->nom }}</h5>
+                            {{-- <h6 class="card-subtitle mb-2 text-muted">{{ $producte->descripcio }}</h6> --}}
+                            <a class="btn a-editar" href="{{ route('afegirProducteForm', ['id'=>$producte->id]) }}"> EDITAR </a>
                         </div>
-                        <div class="card-footer text-muted">
-                            Stock: {{ $producte->stock }}
-                        </div>
+                        
+                           
+                        
                     </div>
                 </div>
             @endforeach
